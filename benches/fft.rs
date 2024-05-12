489a2796b6399f59c5d13875a4bee462371ce391
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2024-05-12 21:27:41 +0200

    Adapt to jlrs 0.20

--- benches/fft.rs
+++ benches/fft.rs
@@ -1,7 +1,7 @@
 #[cfg(feature = "rt")]
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 #[cfg(feature = "rt")]
-use jlrs::{data::managed::array::TypedRankedArray, memory::target::frame::GcFrame, prelude::*};
+use jlrs::{data::{managed::array::TypedRankedArray, layout::complex::Complex}, memory::target::frame::LocalGcFrame, prelude::*};
 #[cfg(feature = "rt")]
 use pprof::{
     criterion::{Output, PProfProfiler},
@@ -9,39 +9,38 @@ use pprof::{
 };
 
 #[cfg(feature = "rt")]
-use rustfft_jl::{FftInstance, FftPlanner, JuliaComplex};
+use rustfft_jl::{FftInstance, FftPlanner};
 
 #[cfg(feature = "rt")]
 #[inline(never)]
-fn bench_forward_fft64(frame: &mut GcFrame, c: &mut Criterion) {
+fn bench_forward_fft64(frame: &LocalGcFrame<0>, c: &mut Criterion) {
     let mut group = c.benchmark_group("Forward FFT64");
     for sz in [
         2, 3, 4, 5, 7, 8, 11, 13, 16, 17, 19, 23, 29, 31, 32, 37, 41, 43, 47, 53, 59, 61, 64, 67,
         71, 73, 79, 83, 89, 97, 128, 256, 512, 1024, 2048, 4096, 8192,
     ] {
         frame
-            .scope(|mut frame| unsafe {
-                let mut planner = FftPlanner::<JuliaComplex<f64>>::new().root(&mut frame);
+            .local_scope::<_, 3>(|mut frame| unsafe {
+                let mut planner = FftPlanner::<Complex<f64>>::new().into_typed_value().root(&mut frame);
                 let instance = planner
                     .track_exclusive()
                     .unwrap()
                     .plan_fft_forward(sz)
+                    .into_typed_value()
                     .root(&mut frame)
                     .data_ptr()
-                    .cast::<FftInstance<JuliaComplex<f64>>>()
+                    .cast::<FftInstance<Complex<f64>>>()
                     .as_ref();
 
-                let array = Value::eval_string(&mut frame, format!("ones(ComplexF64, {sz})"))
+                let array = Value::eval_string(&mut frame, format!("rand(ComplexF64, {sz})"))
                     .unwrap()
-                    .cast_unchecked::<TypedRankedArray<JuliaComplex<f64>, 1>>();
+                    .cast_unchecked::<TypedRankedArray<Complex<f64>, 1>>();
 
                 group.throughput(Throughput::Elements(sz as u64));
                 group.bench_with_input(BenchmarkId::from_parameter(sz), &sz, |b, _sz| {
                     b.iter(|| black_box(instance.process_unchecked(array)))
                 });
-                Ok(())
-            })
-            .unwrap();
+            });
     }
 
     group.finish()
@@ -49,35 +48,34 @@ fn bench_forward_fft64(frame: &mut GcFrame, c: &mut Criterion) {
 
 #[cfg(feature = "rt")]
 #[inline(never)]
-fn bench_forward_fft32(frame: &mut GcFrame, c: &mut Criterion) {
+fn bench_forward_fft32(frame: &LocalGcFrame<0>, c: &mut Criterion) {
     let mut group = c.benchmark_group("Forward FFT32");
     for sz in [
         2, 3, 4, 5, 7, 8, 11, 13, 16, 17, 19, 23, 29, 31, 32, 37, 41, 43, 47, 53, 59, 61, 64, 67,
         71, 73, 79, 83, 89, 97, 128, 256, 512, 1024, 2048, 4096, 8192,
     ] {
         frame
-            .scope(|mut frame| unsafe {
-                let mut planner = FftPlanner::<JuliaComplex<f32>>::new().root(&mut frame);
+            .local_scope::<_, 3>(|mut frame| unsafe {
+                let mut planner = FftPlanner::<Complex<f32>>::new().into_typed_value().root(&mut frame);
                 let instance = planner
                     .track_exclusive()
                     .unwrap()
                     .plan_fft_forward(sz)
+                    .into_typed_value()
                     .root(&mut frame)
                     .data_ptr()
-                    .cast::<FftInstance<JuliaComplex<f32>>>()
+                    .cast::<FftInstance<Complex<f32>>>()
                     .as_ref();
 
-                let array = Value::eval_string(&mut frame, format!("ones(ComplexF32, {sz})"))
+                let array = Value::eval_string(&mut frame, format!("rand(ComplexF32, {sz})"))
                     .unwrap()
-                    .cast_unchecked::<TypedRankedArray<JuliaComplex<f32>, 1>>();
+                    .cast_unchecked::<TypedRankedArray<Complex<f32>, 1>>();
 
                 group.throughput(Throughput::Elements(sz as u64));
                 group.bench_with_input(BenchmarkId::from_parameter(sz), &sz, |b, _sz| {
                     b.iter(|| black_box(instance.process_unchecked(array)))
                 });
-                Ok(())
-            })
-            .unwrap();
+            });
     }
 
     group.finish()
@@ -86,20 +84,15 @@ fn bench_forward_fft32(frame: &mut GcFrame, c: &mut Criterion) {
 #[cfg(feature = "rt")]
 fn criterion_benchmark(c: &mut Criterion) {
     unsafe {
-        let mut frame = StackFrame::new();
-        let mut julia = RuntimeBuilder::new().start().unwrap();
-        let mut julia = julia.instance(&mut frame);
+        let julia = Builder::new().start_local().unwrap();
 
         julia
-            .scope(|mut frame| {
+            .local_scope::<_, 0>(|frame| {
                 // Manually call the init function so all exported types are initialized.
                 rustfft_jl::rustfft_jl_init(Module::main(&frame), 1);
-                bench_forward_fft64(&mut frame, c);
-                bench_forward_fft32(&mut frame, c);
-
-                Ok(())
-            })
-            .unwrap();
+                bench_forward_fft64(&frame, c);
+                bench_forward_fft32(&frame, c);
+            });
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,9 @@
 use std::sync::Arc;
 
 use jlrs::{
-    convert::compatible::{Compatible, CompatibleCast},
     data::{
-        layout::valid_layout::ValidField,
-        managed::{
-            array::TypedRankedArray,
-            value::typed::{TypedValue, TypedValueRet},
-        },
+        layout::{complex::Complex, valid_layout::ValidField},
+        managed::{array::TypedRankedArray, ccall_ref::CCallRefRet, value::typed::TypedValue},
         types::{
             construct_type::ConstructType,
             foreign_type::{ParametricBase, ParametricVariant},
@@ -33,10 +29,7 @@ use jlrs::{
     prelude::*,
 };
 
-use rustfft::{
-    num_complex::{self},
-    Fft, FftNum, FftPlanner as FftPlannerImp,
-};
+use rustfft::{Fft, FftNum, FftPlanner as FftPlannerImp};
 
 /// Dummy implementation for _Unwind_Resume
 ///
@@ -49,43 +42,21 @@ use rustfft::{
 #[no_mangle]
 unsafe extern "C" fn _Unwind_Resume() {}
 
-/// Layout for `Base.Complex`, generated with Jlrs.Reflect:
-///
-/// ```ignore
-/// using Jlrs.Reflect
-/// layout = reflect([Complex])
-/// renamestruct!(layout, Complex, "JuliaComplex")
-/// layout
-/// ```
-#[repr(C)]
-#[derive(
-    Clone, Debug, Unbox, ValidLayout, Typecheck, ValidField, ConstructType, CCallArg, CCallReturn,
-)]
-#[jlrs(julia_type = "Base.Complex")]
-pub struct JuliaComplex<T> {
-    pub re: T,
-    pub im: T,
-}
-
 // RustFFT uses the floating-point number type as a generic, in Julia we want the full type. This
 // trait connects the complex number type to its inner type.
 pub trait IsComplex: 'static + Send + Sync {
     type Inner: FftNum;
 }
 
-impl<T: FftNum> IsComplex for JuliaComplex<T> {
+impl<T: FftNum> IsComplex for Complex<T> {
     type Inner = T;
 }
-
-// Safety: `num_complex::Complex` which is compatible with `JuliaComplex`, both are repr(C) and
-// have the same field layout.
-unsafe impl<T> Compatible<num_complex::Complex<T>> for JuliaComplex<T> where T: ValidField + Clone {}
 
 /// Wrapper around `FftPlanner` from RustFFT.
 pub struct FftPlanner<T: IsComplex>(FftPlannerImp<T::Inner>);
 
 unsafe impl<T: IsComplex> ParametricBase for FftPlanner<T> {
-    type Key = FftPlanner<JuliaComplex<f32>>;
+    type Key = FftPlanner<Complex<f32>>;
     impl_type_parameters!('T');
 }
 
@@ -103,41 +74,41 @@ where
 {
     /// Creates a new instance of `FftPlanner`,
     #[inline]
-    pub fn new() -> TypedValueRet<Self> {
+    pub fn new() -> CCallRefRet<Self> {
         // Safety: this function is called through `ccall`, the leaked data is returned
         // immediately after it has been constructed so there's no reason to root it.
         unsafe {
             CCall::stackless_invoke(|unrooted| {
                 let planner = FftPlanner(FftPlannerImp::new());
-                TypedValue::new(unrooted, planner).leak()
+                CCallRefRet::new(TypedValue::new(unrooted, planner).leak())
             })
         }
     }
 
     /// Returns an `FftInstance` that computes forward FFTs of size `len`.
     #[inline]
-    pub fn plan_fft_forward(&mut self, len: usize) -> TypedValueRet<FftInstance<T>> {
+    pub fn plan_fft_forward(&mut self, len: usize) -> CCallRefRet<FftInstance<T>> {
         // Safety: this function is called through `ccall`, no other instances are created, and
         // the leaked data is returned immediately.
         unsafe {
             CCall::stackless_invoke(|unrooted| {
                 let instance = self.0.plan_fft_forward(len);
                 let instance = FftInstance(instance);
-                TypedValue::new(unrooted, instance).leak()
+                CCallRefRet::new(TypedValue::new(unrooted, instance).leak())
             })
         }
     }
 
     /// Returns an `FftInstance` that computes inverse FFTs of size `len`.
     #[inline]
-    pub fn plan_fft_inverse(&mut self, len: usize) -> TypedValueRet<FftInstance<T>> {
+    pub fn plan_fft_inverse(&mut self, len: usize) -> CCallRefRet<FftInstance<T>> {
         // Safety: this function is called through `ccall`, no other instances are created, and
         // the leaked data is returned immediately.
         unsafe {
             CCall::stackless_invoke(|unrooted| {
                 let instance = self.0.plan_fft_inverse(len);
                 let instance = FftInstance(instance);
-                TypedValue::new(unrooted, instance).leak()
+                CCallRefRet::new(TypedValue::new(unrooted, instance).leak())
             })
         }
     }
@@ -145,7 +116,7 @@ where
 
 pub struct FftInstance<T: IsComplex>(Arc<dyn Fft<T::Inner>>);
 unsafe impl<T: IsComplex> ParametricBase for FftInstance<T> {
-    type Key = FftInstance<JuliaComplex<f32>>;
+    type Key = FftInstance<Complex<f32>>;
     impl_type_parameters!('T');
 }
 
@@ -158,7 +129,8 @@ where
 
 impl<T> FftInstance<T>
 where
-    T: IsComplex + ValidField + Clone + ConstructType + Compatible<num_complex::Complex<T::Inner>>,
+    T: IsComplex + Clone,
+    T::Inner: jlrs::data::layout::is_bits::IsBits + ConstructType + ValidField,
     Self: ParametricVariant,
 {
     /// Returns the length required by this instance.
@@ -176,16 +148,14 @@ where
     /// another thread while this function is called. The buffer is tracked, but that only
     /// guarantees Rust functions are aware the data is mutably borrowed.
     #[inline]
-    pub unsafe fn process(&self, buffer: TypedRankedArray<T, 1>) -> JlrsResult<()> {
-        let mut arr = buffer.as_typed();
+    pub unsafe fn process(&self, buffer: TypedRankedArray<Complex<T::Inner>, 1>) -> JlrsResult<()> {
         // Try to mutably track the buffer. Return a `BorrowError` if this fails.
-        let mut tracked_buffer = arr.track_exclusive()?;
+        let mut tracked_buffer = buffer.track_exclusive()?;
 
         // Access the array as a mutable slice and convert its type to the compatible
         // `num_complex::Complex`.
-        let slice = tracked_buffer
-            .as_mut_slice()
-            .compatible_cast_mut::<num_complex::Complex<T::Inner>>();
+        let mut slice = tracked_buffer.bits_data_mut();
+        let slice = slice.as_mut_slice();
 
         // https://docs.rs/rustfft/6.1.0/src/rustfft/lib.rs.html#183
         // This method panics if:
@@ -211,12 +181,12 @@ where
     /// Safety: the contents of `buffer` are mutated, you must guarantee it isn't accessed from
     /// another thread while this function is called. The buffer is not tracked.
     #[inline]
-    pub unsafe fn process_untracked(&self, buffer: TypedRankedArray<T, 1>) -> JlrsResult<()> {
-        let mut buffer = buffer.as_typed();
-        let mut accessor = buffer.bits_data_mut().unwrap_unchecked();
-        let slice = accessor
-            .as_mut_slice()
-            .compatible_cast_mut::<num_complex::Complex<T::Inner>>();
+    pub unsafe fn process_untracked(
+        &self,
+        mut buffer: TypedRankedArray<Complex<T::Inner>, 1>,
+    ) -> JlrsResult<()> {
+        let mut accessor = buffer.bits_data_mut();
+        let slice = accessor.as_mut_slice();
 
         // https://docs.rs/rustfft/6.1.0/src/rustfft/lib.rs.html#183
         // This method panics if:
@@ -242,12 +212,9 @@ where
     /// Safety: the contents of `buffer` are mutated, you must guarantee it isn't accessed from
     /// another thread while this function is called. The buffer is not tracked.
     #[inline]
-    pub unsafe fn process_unchecked(&self, buffer: TypedRankedArray<T, 1>) {
-        let mut buffer = buffer.as_typed();
-        let mut accessor = buffer.bits_data_mut().unwrap_unchecked();
-        let slice = accessor
-            .as_mut_slice()
-            .compatible_cast_mut::<num_complex::Complex<T::Inner>>();
+    pub unsafe fn process_unchecked(&self, mut buffer: TypedRankedArray<Complex<T::Inner>, 1>) {
+        let mut accessor = buffer.bits_data_mut();
+        let slice = accessor.as_mut_slice();
         self.0.process(slice);
     }
 }
@@ -255,7 +222,7 @@ where
 julia_module! {
     become rustfft_jl_init;
 
-    for T in [JuliaComplex<f32>, JuliaComplex<f64>] {
+    for T in [Complex<f32>, Complex<f64>] {
         #[doc = "    FftPlanner{T}"]
         #[doc = ""]
         #[doc = "A planner for forward and inverse FFTs of `Vector{T}`, `T` must be `ComplexF32`"]
@@ -267,25 +234,25 @@ julia_module! {
         #[doc = ""]
         #[doc = "Plan a forward FFT for `Vector{T}` data of length `len`. If the planner is"]
         #[doc = "already tracked `JlrsCore.BorrowError` is thrown."]
-        in FftPlanner<T> fn plan_fft_forward(&mut self, len: usize) -> TypedValueRet<FftInstance<T>> as rustfft_plan_fft_forward!;
+        in FftPlanner<T> fn plan_fft_forward(&mut self, len: usize) -> CCallRefRet<FftInstance<T>> as rustfft_plan_fft_forward!;
 
         #[doc = "    rustfft_plan_fft_forward_untracked!(planner::FftPlanner{T}, len::UInt)"]
         #[doc = ""]
         #[doc = "Plan a forward FFT for `Vector{T}` data of length `len` without tracking the planner."]
         #[untracked_self]
-        in FftPlanner<T> fn plan_fft_forward(&mut self, len: usize) -> TypedValueRet<FftInstance<T>> as rustfft_plan_fft_forward_untracked!;
+        in FftPlanner<T> fn plan_fft_forward(&mut self, len: usize) -> CCallRefRet<FftInstance<T>> as rustfft_plan_fft_forward_untracked!;
 
         #[doc = "    rustfft_plan_fft_inverse!(planner::FftPlanner{T}, len::UInt)"]
         #[doc = ""]
         #[doc = "Plan an inverse FFT for `Vector{T}` data of length `len`. If the planner is"]
         #[doc = "already tracked `JlrsCore.BorrowError` is thrown."]
-        in FftPlanner<T> fn plan_fft_inverse(&mut self, len: usize) -> TypedValueRet<FftInstance<T>> as rustfft_plan_fft_inverse!;
+        in FftPlanner<T> fn plan_fft_inverse(&mut self, len: usize) -> CCallRefRet<FftInstance<T>> as rustfft_plan_fft_inverse!;
 
         #[doc = "    rustfft_plan_fft_inverse_untracked!(planner::FftPlanner{T}, len::UInt)"]
         #[doc = ""]
         #[doc = "Plan an inverse FFT for `Vector{T}` data of length `len` without tracking the planner."]
         #[untracked_self]
-        in FftPlanner<T> fn plan_fft_inverse(&mut self, len: usize) -> TypedValueRet<FftInstance<T>> as rustfft_plan_fft_inverse_untracked!;
+        in FftPlanner<T> fn plan_fft_inverse(&mut self, len: usize) -> CCallRefRet<FftInstance<T>> as rustfft_plan_fft_inverse_untracked!;
 
         #[doc = "    FftInstance{T}"]
         #[doc = ""]
@@ -345,16 +312,16 @@ julia_module! {
         in FftInstance<T> fn process_unchecked(&self, buffer: TypedRankedArray<T, 1>) as rustfft_fft_unchecked_gcsafe!;
     };
 
-    type FftPlanner32 = FftPlanner<JuliaComplex<f32>>;
+    type FftPlanner32 = FftPlanner<Complex<f32>>;
 
     #[doc = "    FftPlanner32"]
     #[doc = ""]
     #[doc = "A planner for single-precision complex data."]
-    in FftPlanner<JuliaComplex<f32>> fn new() -> TypedValueRet<FftPlanner<JuliaComplex<f32>>> as FftPlanner32;
+    in FftPlanner<Complex<f32>> fn new() -> CCallRefRet<FftPlanner<Complex<f32>>> as FftPlanner32;
 
-    type FftPlanner64 = FftPlanner<JuliaComplex<f64>>;
+    type FftPlanner64 = FftPlanner<Complex<f64>>;
     #[doc = "    FftPlanner64"]
     #[doc = ""]
     #[doc = "A planner for double-precision complex data."]
-    in FftPlanner<JuliaComplex<f64>> fn new() -> TypedValueRet<FftPlanner<JuliaComplex<f64>>> as FftPlanner64;
+    in FftPlanner<Complex<f64>> fn new() -> CCallRefRet<FftPlanner<Complex<f64>>> as FftPlanner64;
 }
